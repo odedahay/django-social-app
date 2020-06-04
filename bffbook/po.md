cd554a1ce54c5a8b545ec3f3a4141b32d49b89d8
Ode <odedahay@yahoo.com>
2020-06-05 05:42:49 +0800

    updated profiles

---
+++ bffbook/po.md
@@ -0,0 +1,7 @@
+1. profiles
+    - profile
+2. posts
+    - Post
+    - Comment
+    - Like
+3. allauth (authentication)
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/odedahay/Documents/Projects/django-projects/django-social-app/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
